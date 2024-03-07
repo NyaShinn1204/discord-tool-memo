828d379586638bd97b92855c40a23242f5fc878d
NyaShinn1204 <134133778+NyaShinn1204@users.noreply.github.com>
2024-03-07 22:07:38 +0900

    Update get_cookie.py

--- get_cookie.py
+++ get_cookie.py
@@ -1,9 +1,6 @@
-import get_session as get_session
+import requests
 
-session = get_session.get_session()
-
-def get_cookies(session):
-    cookies = dict(session.get("https://discord.com/api/v9/experiments").cookies)
-    cookies["__cf_bm"]="0duPxpWahXQbsel5Mm.XDFj_eHeCKkMo.T6tkBzbIFU-1679837601-0-AbkAwOxGrGl9ZGuOeBGIq4Z+ss0Ob5thYOQuCcKzKPD2xvy4lrAxEuRAF1Kopx5muqAEh2kLBLuED6s8P0iUxfPo+IeQId4AS3ZX76SNC5F59QowBDtRNPCHYLR6+2bBFA=="
-    cookies["locale"]="ja-JP"
-    return cookies
+def get_cookie():
+    session = requests.Session()
+    response = session.get("https://discord.com/api/v9/experiments").cookies
+    return f'__dcfduid={response["__dcfduid"]}; __sdcfduid={response["__sdcfduid"]}; locale=ja-JP'
